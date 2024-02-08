ece1d82e5c7ba653115f08f77e3888b6f5c0929e
fast-malid <157091456+fast-malid@users.noreply.github.com>
2024-02-08 14:31:01 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kimmyve/vegax7",
+    "https://github.com/fast-malid/oekdidnd_odmd",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
